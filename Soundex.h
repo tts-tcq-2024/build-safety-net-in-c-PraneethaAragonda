2cb9eb5c846cc3ce453c3edaf9b787b77cd08ac8
PraneethaAragonda <praneethaaragonda2@gmail.com>
2024-09-02 21:45:08 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,10 +1,8 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
-
 #include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
-
 char getSoundexCode(char c) {
     static const char soundexTable[26] = {'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5','5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
     };
@@ -15,17 +13,14 @@ char getSoundexCode(char c) {
     }
      return soundexTable[c - 'A'];
 }
-
 void initializeSoundex(char *soundex, char firstCharacter) {
     soundex[0] = toupper(firstCharacter);
     soundex[1] = soundex[2] = soundex[3] = '0';
     soundex[4] = '\0';
 }
-
 int shouldAddToSoundex(char code, char *soundex, int sIndex) {
     return sIndex < 4 && code != '0' && code != soundex[sIndex - 1];
 }
-
 void processCharacter(const char *name, char *soundex, int *sIndex, int i) {
     char code = getSoundexCode(name[i]);
     if (shouldAddToSoundex(code, soundex, *sIndex)) {
@@ -33,7 +28,6 @@ void processCharacter(const char *name, char *soundex, int *sIndex, int i) {
         (*sIndex)++;
     }
 }
-
 
 void generateSoundex(const char *name, char *soundex) {
     initializeSoundex(soundex, name[0]);
@@ -43,5 +37,4 @@ void generateSoundex(const char *name, char *soundex) {
         processCharacter(name, soundex, &sIndex, i);
     }
 }
-
 #endif // SOUNDEX_H
